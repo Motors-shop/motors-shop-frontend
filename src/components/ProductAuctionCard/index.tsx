4c1c8021b59b9608d9bf8d981546e18b10c94e2e
andrrms <andremarcia4@gmail.com>
2022-12-21 01:58:17 -0400

    feat: adicionado card de leilão de produto

---
+++ src/components/ProductAuctionCard/index.tsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import { FiChevronRight, FiClock } from "react-icons/fi";
+
+import ThemeButton from "../ThemeButton";
+import ThemeLinkButton from "../ThemeLinkButton";
+import UserChip from "../UserChip";
+
+import {
+  ContentContainer,
+  CoverContainer,
+  Content,
+  ProductAuctionCardContainer,
+  ProductDescription,
+  ProductFooter,
+  ProductPrice,
+  ProductTitle,
+  TagsList,
+  TimeBadge,
+} from "./style";
+import { IProductAuctionProps } from "./types";
+
+const ProductAuctionCard: React.FC<IProductAuctionProps> = ({
+  coverImage,
+  title,
+  description,
+  owner,
+  tags,
+  price,
+  dueDate,
+  ...linkProps
+}) => {
+  const [nowTime, setNowTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNowTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [nowTime]);
+
+  return (
+    <ProductAuctionCardContainer>
+      <CoverContainer>
+        <img src={coverImage} alt={`${title} product in auction`} />
+        <ContentContainer>
+          <TimeBadge>
+            <FiClock />
+            <span>
+              {new Intl.DateTimeFormat("pt-BR", {
+                hour: "numeric",
+                minute: "numeric",
+                second: "numeric",
+              }).format(dueDate.getTime() - nowTime.getTime())}
+            </span>
+          </TimeBadge>
+          <Content>
+            <ProductTitle>{title}</ProductTitle>
+            <ProductDescription>{description}</ProductDescription>
+            <UserChip name={owner.name} avatar={owner.avatar} whiteText />
+            <ProductFooter>
+              {tags && (
+                <TagsList>
+                  {tags.map((tag, i) => (
+                    <li key={i}>
+                      <ThemeButton variant="primaryLight">{tag}</ThemeButton>
+                    </li>
+                  ))}
+                </TagsList>
+              )}
+              <ProductPrice>
+                {price.toLocaleString("pt-BR", {
+                  currency: "BRL",
+                  style: "currency",
+                })}
+              </ProductPrice>
+            </ProductFooter>
+          </Content>
+        </ContentContainer>
+      </CoverContainer>
+      <ThemeLinkButton extend large variant="primary" {...linkProps}>
+        Acessar página do leilão <FiChevronRight />
+      </ThemeLinkButton>
+    </ProductAuctionCardContainer>
+  );
+};
+
+export default ProductAuctionCard;
---
+++ src/components/ProductAuctionCard/style.ts
@@ -0,0 +1,147 @@
+import styled from "styled-components";
+
+import { Btn as ButtonBtn } from "../ThemeButton/style";
+import { Btn as LinkBtn } from "../ThemeLinkButton/style";
+
+export const ProductAuctionCardContainer = styled.div`
+  max-width: 735px;
+
+  border-radius: 8px;
+  overflow: hidden;
+
+  ${LinkBtn} {
+    border-radius: 0;
+
+    justify-content: space-between;
+
+    padding: 15px 36px;
+  }
+`;
+
+export const CoverContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 326px;
+
+  background-color: var(--grey7);
+
+  display: flex;
+  justify-content: center;
+
+  img {
+    height: 100%;
+  }
+`;
+
+export const ContentContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 100%;
+
+  padding: 24px 36px;
+
+  background-color: rgba(0, 0, 0, 0.6);
+  background: linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.4));
+  background-size: 100% 150%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  transition: 1s ease background-position;
+
+  ${ProductAuctionCardContainer}:hover & {
+    background-position: 150%;
+  }
+`;
+
+export const TimeBadge = styled.div`
+  background-color: var(--whiteFixed);
+  color: var(--grey1);
+  border-radius: 100px;
+
+  font-family: var(--lexend);
+  font-size: 1rem;
+  font-weight: 500;
+
+  width: fit-content;
+  padding: 8px;
+
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  span {
+    padding-right: 8px;
+  }
+`;
+
+export const Content = styled.div`
+  margin-top: auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const ProductTitle = styled.h3`
+  color: var(--whiteFixed);
+
+  font-family: var(--lexend);
+  font-weight: 600;
+  font-size: 1.2rem;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export const ProductDescription = styled.p`
+  color: var(--grey5);
+
+  display: -webkit-box;
+
+  font-family: var(--inter);
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 1.2rem;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+
+  height: 1.1rem;
+
+  overflow: hidden;
+
+  transition: 0.3s ease height;
+
+  ${ProductAuctionCardContainer}:hover & {
+    -webkit-line-clamp: 4;
+    height: calc(1.2rem * 4);
+  }
+`;
+
+export const ProductFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TagsList = styled.ul`
+  display: flex;
+  flex-flow: row wrap;
+  gap: 10px;
+
+  ${ButtonBtn} {
+    font-size: 0.8rem;
+    padding: 8px 10px;
+  }
+`;
+
+export const ProductPrice = styled.p`
+  font-family: var(--lexend);
+  font-weight: 500;
+  font-size: 1rem;
+
+  color: var(--whiteFixed);
+`;
