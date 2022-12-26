2de09a161b81dc39b95db54dde160d2b19033a6a
João Pedro Porto Teixeira <joao.porto.ismart@gmail.com>
2022-12-26 03:23:46 -0300

    feat: Seller Products body

---
+++ src/pages/SellerProducts/index.tsx
@@ -0,0 +1,15 @@
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+import { StyledBody } from "./styles";
+
+const SellerProducts = () => {
+  return (
+    <>
+      <Navbar />
+      <StyledBody></StyledBody>
+      <Footer />
+    </>
+  );
+};
+
+export default SellerProducts;
