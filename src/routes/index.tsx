5d2e8ebeade9dacb6fcd08a17da7d21909a13c1e
Derick <d.a.s2000silva14@gmail.com>
2022-12-27 17:38:33 -0300

    feat: página de erro 404

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Routes as Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import NotFound from "../pages/NotFound";
 import Product from "../pages/Product";
 
 const Routes = () => {
@@ -11,7 +12,7 @@ const Routes = () => {
       <Route path="/login" element={<Login />} />
       {/* <Route path="/register" element={} /> */}
       {/* <Route path="/:user_id/products" element={} /> */}
-      {/* <Route path="*" element={} /> */}
+      <Route path="*" element={<NotFound />} />
     </Switch>
   );
 };
