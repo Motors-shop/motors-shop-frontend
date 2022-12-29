5e62ecfaefeee2a9522df0db69575db84d143b37
Derick <d.a.s2000silva14@gmail.com>
2022-12-29 14:10:07 -0300

    feat: atualização dos comentários ao adicionar um novo

--- src/components/CommentCard/index.tsx
+++ src/components/CommentCard/index.tsx
@@ -1,18 +1,52 @@
+import { IProductCommentProps } from "../../pages/Product/Comments/types";
 import UserChip from "../UserChip";
 import { ThemeCard } from "./style";
 
-const CommentCard = () => {
+const CommentCard = ({ user, commentary, createdAt }: IProductCommentProps) => {
+  const time = new Date().getTime() - new Date(createdAt).getTime();
+  const seconds = Math.floor(time / 1000);
+  const minutes = Math.floor(time / 60000);
+  const hours = Math.floor(time / 3.6e6);
+  const days = Math.floor(time / 8.64e7);
+  const months = new Date().getMonth() - new Date(createdAt).getMonth();
+  const years = new Date().getFullYear() - new Date(createdAt).getFullYear();
+
   return (
     <ThemeCard>
       <div>
-        <UserChip name="Samuel Leão" />
-        <span>há 3 dias</span>
+        <UserChip name={user} />
+        {seconds < 60 && (
+          <span>
+            há {seconds} {seconds === 1 ? "segundo" : "segundos"}
+          </span>
+        )}
+        {minutes < 60 && minutes !== 0 && (
+          <span>
+            há {minutes} {minutes === 1 ? "minuto" : "minutos"}
+          </span>
+        )}
+        {minutes > 60 && hours < 24 && (
+          <span>
+            há {hours} {hours === 1 ? "hora" : "horas"}
+          </span>
+        )}
+        {hours >= 24 && months === 0 && (
+          <span>
+            há {days} {days === 1 ? "dia" : "dias"}
+          </span>
+        )}
+        {months > 0 && years === 0 && (
+          <span>
+            há {months} {months === 1 ? "mês" : "meses"}
+          </span>
+        )}
+        {years > 0 && (
+          <span>
+            há {years} {years === 1 ? "ano" : "anos"}
+          </span>
+        )}
       </div>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-        galley of type and scrambled it to make a type specimen book.
-      </p>
+      <p>{commentary}</p>
     </ThemeCard>
   );
 };
