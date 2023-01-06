f62e15176d3dc14eedbf0a0031527852a6dfd323
João Pedro Porto Teixeira <joao.porto.ismart@gmail.com>
2023-01-06 12:38:06 -0300

    feat: edit products

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../contexts/UserProvider";
+import EditVehicle from "../EditVehicle";
+import Modal, { useModalControls } from "../Modal";
 
 import ThemeButton from "../ThemeButton";
 import UserChip from "../UserChip";
@@ -12,6 +14,7 @@ import {
   ProductFooter,
   ProductPrice,
   ProductTitle,
+  StyledAdminButtons,
   TagList,
 } from "./style";
 import { IProductCardProps } from "./types";
@@ -28,40 +31,63 @@ const ProductCard: React.FC<IProductCardProps> = ({
   ...linkProps
 }) => {
   const { user } = useContext(UserContext);
+  const { openModal } = useModalControls();
 
   return (
-    <ProductCardContainer {...linkProps} draggable="false">
-      <CoverContainer>
-        <img src={coverImage} alt={`${title} product`} draggable="false" />
-        {isOwner && (
-          <OwnerBadge isPublished={isPublished}>{isPublished ? "Ativo" : "Inativo"}</OwnerBadge>
+    <>
+      <Modal name="editVehicle" title="Editar anúncio">
+        <EditVehicle />
+      </Modal>
+
+      <ProductCardContainer {...linkProps} draggable="false">
+        <CoverContainer>
+          <img src={coverImage} alt={`${title} product`} draggable="false" />
+          {isOwner && (
+            <OwnerBadge isPublished={isPublished}>
+              {isPublished ? "Ativo" : "Inativo"}
+            </OwnerBadge>
+          )}
+        </CoverContainer>
+        <ProductTitle>{title}</ProductTitle>
+        <ProductDescription>{description}</ProductDescription>
+        <UserChip
+          name={owner.name}
+          avatar={owner.avatar}
+          user={user.id === owner.id ? true : false}
+        />
+        <ProductFooter>
+          {tags && (
+            <TagList>
+              {tags.map((tag, i) => (
+                <li key={i}>
+                  <ThemeButton variant="primaryLight">{tag}</ThemeButton>
+                </li>
+              ))}
+            </TagList>
+          )}
+          <ProductPrice>
+            {price.toLocaleString("pt-BR", {
+              currency: "BRL",
+              style: "currency",
+            })}
+          </ProductPrice>
+        </ProductFooter>
+        {user.id === owner.id && (
+          <StyledAdminButtons>
+            <ThemeButton
+              variant="normal"
+              outlined={true}
+              onClick={() => openModal("editVehicle")}
+            >
+              Editar
+            </ThemeButton>
+            <ThemeButton variant="normal" outlined={true}>
+              Ver como
+            </ThemeButton>
+          </StyledAdminButtons>
         )}
-      </CoverContainer>
-      <ProductTitle>{title}</ProductTitle>
-      <ProductDescription>{description}</ProductDescription>
-      <UserChip
-        name={owner.name}
-        avatar={owner.avatar}
-        user={user.id === owner.id ? true : false}
-      />
-      <ProductFooter>
-        {tags && (
-          <TagList>
-            {tags.map((tag, i) => (
-              <li key={i}>
-                <ThemeButton variant="primaryLight">{tag}</ThemeButton>
-              </li>
-            ))}
-          </TagList>
-        )}
-        <ProductPrice>
-          {price.toLocaleString("pt-BR", {
-            currency: "BRL",
-            style: "currency",
-          })}
-        </ProductPrice>
-      </ProductFooter>
-    </ProductCardContainer>
+      </ProductCardContainer>
+    </>
   );
 };
 
